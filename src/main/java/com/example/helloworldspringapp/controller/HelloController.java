675326d742869b0abf2e4142179a9998b82cc858
naveenkumargorakala <nicenaveenkumargorakala@gmail.com>
2023-06-26 19:55:45 -0700

    Add:Create REST Controller to shows in web

--- src/main/java/com/example/helloworldspringapp/controller/HelloController.java
+++ src/main/java/com/example/helloworldspringapp/controller/HelloController.java
@@ -1,9 +1,8 @@
 package com.example.helloworldspringapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 public class HelloController {
 
 
